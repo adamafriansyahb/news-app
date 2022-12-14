3707bcf3940bbd0a783f64ac8d88c3319cf8e966
adamafriansyahb <adamafriansyahb@gmail.com>
2022-12-14 09:44:18 +0700

    feat: create article card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+import NewsList from './NewsList';
 import { categories } from '../constants';
 import fetchNews from '../lib/fetchNews';
+import mockNewsResponse from '../mockNewsResponse.json';
 
 type Props = {};
 
 const HomePage = async (props: Props) => {
-  const news: NewsResponse = await fetchNews(categories.join(','));
+  const news: NewsResponse =
+    mockNewsResponse || (await fetchNews(categories.join(',')));
 
-  return <div>Home page</div>;
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
 };
 
 export default HomePage;
